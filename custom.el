d4d0fc1cba6422c1935d889b606d1771e6145e1a
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2021-04-22 15:33:04 -0300

    update font-size

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(minimap flycheck-elixir flycheck)))
+ '(package-selected-packages '(flyspell-correct minimap flycheck-elixir flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
