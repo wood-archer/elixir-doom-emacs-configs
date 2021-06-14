b3324160171f613636f5e7cfe46d80596366d249
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2021-06-14 13:52:14 -0300

    update settings

--- config.el
+++ config.el
@@ -27,14 +27,14 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (setq display-line-numbers-type nil)
-(setq doom-font (font-spec :family "MonacoB2" :size 11 :weight 'bold))
+(setq doom-font (font-spec :family "MonacoB2" :size 10 :weight 'bold))
 
 (setq lsp-clients-elixir-server-executable "~/elixir-ls/release/language_server.sh")
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-tomorrow-night)
+(setq doom-theme 'doom-dracula)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
